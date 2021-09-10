e1412feb7b0d2de2fdb258222b1803381c495231
hau.mai <hau.mai@hdwebsoft.co>
2021-09-10 11:09:32 +0700

    add PORT

--- src/main.js
+++ src/main.js
@@ -6,3 +6,6 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+App.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
